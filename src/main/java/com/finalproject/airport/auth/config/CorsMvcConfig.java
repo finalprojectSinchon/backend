c68912bb42f0854d92e2d6f492071e9c16098245
yoonjae <realyoon77@gmail.com>
2024-07-17 16:48:37 +0900

    [2024-07-17] FileUpload Util 클래스 추가

--- src/main/java/com/finalproject/airport/auth/config/CorsMvcConfig.java
+++ src/main/java/com/finalproject/airport/auth/config/CorsMvcConfig.java
@@ -1,14 +1,30 @@
 package com.finalproject.airport.auth.config;
 
 import com.finalproject.airport.auth.filter.HeaderFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
+
+    // 정적 자원에 접근을 허용하게 하기 위함
+    @Value("${image.add-resource-locations}")
+    private String ADD_RESOURCE_LOCATION;
+
+    @Value("${image.add-resource-handler}")
+    private String ADD_RESOURCE_HANDLER;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler(ADD_RESOURCE_HANDLER)
+                .addResourceLocations(ADD_RESOURCE_LOCATION);
+    }
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
