deb4198b2987b27ee84fc0f1cbbdb60f75eb3c85
gony <goeun990@naver.com>
2024-07-22 12:08:51 +0900

    [2024-07-11] 정비 삭제

--- src/main/java/com/finalproject/airport/maintenance/repository/MaintenanceRepository.java
+++ src/main/java/com/finalproject/airport/maintenance/repository/MaintenanceRepository.java
@@ -3,9 +3,13 @@ package com.finalproject.airport.maintenance.repository;
 import com.finalproject.airport.maintenance.entity.MaintenanceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaintenanceRepository extends JpaRepository<MaintenanceEntity, Integer> {
 
     MaintenanceEntity findById(int maintenanceCode);
 
     MaintenanceEntity findBymaintenanceCode(int maintenanceCode);
+
+
 }
