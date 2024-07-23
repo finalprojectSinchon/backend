07de9f1d0b40062d3b87003e385ae5e02c190330
gony <goeun990@naver.com>
2024-07-23 16:33:29 +0900

    [2024-07-23] 승인 시설문 전체 조회

--- src/main/java/com/finalproject/airport/approval/repository/ApprovalRepository.java
+++ src/main/java/com/finalproject/airport/approval/repository/ApprovalRepository.java
@@ -1,10 +1,19 @@
 package com.finalproject.airport.approval.repository;
 
-import com.finalproject.airport.approval.entity.Approval;
+import com.finalproject.airport.approval.entity.ApprovalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApprovalRepository extends JpaRepository<Approval, Integer> {
+import java.util.List;
 
+public interface ApprovalRepository extends JpaRepository<ApprovalEntity, Integer> {
 
 
 }
+
+/*import org.springframework.data.jpa.repository.JpaRepository;
+import com.finalproject.airport.approval.entity.ApprovalEntity;
+
+public interface ApprovalRepository extends JpaRepository<ApprovalEntity, Long> {
+    // 다른 커스텀 메서드를 추가할 수 있습니다.
+}
+*/
