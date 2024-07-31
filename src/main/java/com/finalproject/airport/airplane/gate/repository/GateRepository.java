f9e37adcca4b00a329cbe60b407c5a26c66df732
Leejieun <84463888+ggeunn@users.noreply.github.com>
2024-07-31 11:44:16 +0900

    Update GateRepository.java
    Signed-off-by: Leejieun <84463888+ggeunn@users.noreply.github.com>

--- src/main/java/com/finalproject/airport/airplane/gate/repository/GateRepository.java
+++ src/main/java/com/finalproject/airport/airplane/gate/repository/GateRepository.java
@@ -3,7 +3,7 @@ package com.finalproject.airport.airplane.gate.repository;
 import com.finalproject.airport.airplane.gate.entity.Gate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 @Repository
