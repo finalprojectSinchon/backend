9724bc1172f526c8ad12f4d2c3894195db78697e
DESKTOP-0CUIJL6\LEE <h2odream@naver.com>
2024-08-05 17:41:24 +0900

    [2024-8-05] 안전 점검 시설물 데이터 끌고오기

--- src/main/java/com/finalproject/airport/inspection/dto/InspectionDTO.java
+++ src/main/java/com/finalproject/airport/inspection/dto/InspectionDTO.java
@@ -1,5 +1,11 @@
 package com.finalproject.airport.inspection.dto;
 
+import com.finalproject.airport.airplane.baggageclaim.dto.BaggageClaimDTO;
+import com.finalproject.airport.airplane.checkincounter.dto.CheckinCounterDTO;
+import com.finalproject.airport.airplane.gate.dto.GateDTO;
+import com.finalproject.airport.facilities.dto.FacilitiesDTO;
+import com.finalproject.airport.storage.dto.StorageDTO;
+import com.finalproject.airport.store.dto.StoreDTO;
 import lombok.*;
 
 import java.util.Date;
@@ -30,4 +36,12 @@ public class InspectionDTO {
 
     private String isActive;
 
+    private String structure;
+
+    private GateDTO gate;
+    private CheckinCounterDTO checkinCounter;
+    private BaggageClaimDTO baggageClaim;
+    private StorageDTO storage;
+    private FacilitiesDTO facilities;
+    private StoreDTO store;
 }
